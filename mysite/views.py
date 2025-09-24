896030b8f63ae2e281785677ac97970c5e138db0
deividusm <davidalejandro.sepulvedapoblete@gmail.com>
2025-09-23 21:28:42 -0300

    enrutamiento entre paginas creado(2) para poder avanzar un poco entre otra interfaz web

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
-import datetime
-from django.template import Template, Context
+from django.shortcuts import render
 
-def saludo(request):
-    doc_externo = open(r"C:\Users\david\Desktop\djangoproject\mysite\plantillas\Untitled1.html")
-    plt = Template(doc_externo.read())
-    doc_externo.close()
-    ctx =Context()
-    documento =plt.render(ctx)
-    return HttpResponse(documento)
+def aprende(request):
+    return render(request, 'aprende.html')
+
+def recicla(request):
+    return render(request, 'recicla.html')
+
+
+
+
