d192d39984c06ace9b9ab014d08624a954df77f8
deividusm <davidalejandro.sepulvedapoblete@gmail.com>
2025-09-24 00:25:07 -0300

    Prototipo proyecto interfaz login

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from mysite.views import aprende, recicla  # Asegúrate de que las vistas sean correctas
 
 urlpatterns = [
+    # Ruta principal, llama a la vista 'recicla'
     path('', recicla, name='recicla'),
+
+    # Ruta para la página de aprendizaje, llama a la vista 'aprende'
     path('aprende/', aprende, name='aprende'),
+
+    # Rutas relacionadas con autenticación de Google usando django-allauth
+    path('accounts/', include('allauth.urls')),  # Maneja login, logout, signup, etc.
 ]
