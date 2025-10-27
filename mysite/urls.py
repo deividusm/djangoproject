5e2ac0b7440caa604e7242a20a4dfb8bbd7acf99
david:p <davidalejandro.sepulvedapoblete@gmail.com>
2025-10-27 20:24:22 -0300

    Restaurar archivos eliminados desde la versión base

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path, include
-from django.shortcuts import redirect
-from mysite.views import hub
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from .views import SubirImagenView  # Solo importa SubirImagenView
+from django.urls import path
+from myapp.views import hello
 
 urlpatterns = [
-    path('', lambda r: redirect('account_login'), name='home'),  # Redirige al login si no está logueado
-    path('hub/', hub, name='hub'),                                # Página principal, requiere login
-    path('accounts/', include('allauth.urls')),                   # Autenticación de usuarios
-    path('admin/', admin.site.urls),                              # Admin de Django
-    path('verificar_mobilenet/', SubirImagenView.as_view(), name='verificar_mobilenet'),  # Solo una vista para subir y verificar la imagen
+    path('admin/', admin.site.urls),
+    path('', hello)
 ]
